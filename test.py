0304ec799b14888e7ef08879e70e6e121a539760
Kubasik <tomkubasik.pro@gmail.com>
2020-05-20 12:50:12 +0200

    call monique and ask her the meteo

---
+++ test.py
@@ -0,0 +1,53 @@
+from gtts import gTTS 
+import playsound
+import speech_recognition as sr
+import os 
+import random
+import webbrowser as wb 
+
+r = sr.Recognizer()
+language = "fr"
+call_assistant = "Monique"
+
+
+def record_audio():
+    with sr.Microphone() as source:
+        print('Ecoute ...')
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio, language="fr-FR")
+        except sr.RequestError:
+            print('Deso pas dispo')
+        except sr.UnknownValueError:
+            print('deso pas compris')
+        return voice_data
+
+
+def google_speak(audio_string):
+    tts = gTTS(text=audio_string, lang = language)
+    r = random.randint(1, 10000000)
+    audio_file = 'audio-' + str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
+
+
+def respond(response):
+    if 'météo' in response:
+        url = 'https://www.google.com/search?q=meteo'
+        wb.get().open(url)
+        google_speak('Voila la météo')
+
+
+while 1:
+    call = record_audio()
+    if call_assistant in call:
+        google_speak('Que puis-je faire pour vous ?')
+        response = record_audio()
+        print(response)
+        respond(response)
+    if 'au revoir' in call:
+        exit()
+
